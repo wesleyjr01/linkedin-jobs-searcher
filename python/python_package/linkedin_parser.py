d2eca503c4ec64e5e12e57fd898c9232ceffeeb8
Wesley Bortolozo <wesleyjr0101@gmail.com>
2024-04-13 18:34:12 -0300

    missing enhancers

--- python/python_package/linkedin_parser.py
+++ python/python_package/linkedin_parser.py
@@ -1,14 +1,37 @@
-from typing import Dict
+import json
+import logging
+from typing import Dict, List
 from linkedin_api import Linkedin
 
 from dotenv import load_dotenv
 import os
-import json
 
 import datetime
+from ratelimit import limits, sleep_and_retry
+
+# Global Variables / API Rate Limit
+period_in_seconds: int = 15
+calls_per_period: int = 1
+
+
+# Define logger
+def get_logger(
+    level: int = logging.INFO,
+    format: str = "%(asctime)s [%(filename)s] [%(funcName)s] [%(levelname)s] %(message)s",
+    logging_file_handler: str = "/tmp/filename.log",
+):
+    handlers = [logging.FileHandler(logging_file_handler), logging.StreamHandler()]
+    logging.basicConfig(level=level, format=format, handlers=handlers)
+    logger = logging.getLogger()
+    logger.setLevel(level)
+    return logger
+
+
+logger = get_logger()
 
 
 class LinkedinJobParser:
+
     def __init__(
         self,
         login_email: str,
@@ -24,10 +47,8 @@ class LinkedinJobParser:
         self._login_password = login_password
         self._listed_at = self.days_old_listed_job * 24 * 60 * 60
         self._api_client = self.get_api_client
-
-    @property
-    def get_api_client(self):
-        return Linkedin(self._login_email, self._login_password)
+        self._jobs = self.get_jobs
+        self._job_ids = self.get_jobs_ids
 
     @staticmethod
     def epoch_to_timestamp(epoch_time_ms):
@@ -39,46 +60,45 @@ class LinkedinJobParser:
         Returns:
             str: Timestamp in 'YYYY-MM-DD HH:mm:ss' format.
         """
-        # Convert milliseconds to seconds
         epoch_time_s = epoch_time_ms / 1000
 
-        # Convert epoch time to a datetime object in UTC
         date_time = datetime.datetime.utcfromtimestamp(epoch_time_s)
 
-        # Format the datetime object as a string in the specified format
         formatted_time = date_time.strftime("%Y-%m-%d %H:%M:%S")
 
         return formatted_time
 
-    def get_information_from_job(self, job_dict: Dict) -> Dict:
-        job_info = {}
-        job_info["job_id"] = job_dict["jobPostingId"]
-        job_info["company_name"] = (
-            job_dict["companyDetails"]
-            .get(
-                "com.linkedin.voyager.deco.jobs.web.shared.WebCompactJobPostingCompany"
-            )
-            .get("companyResolutionResult")
-            .get("name")
-        )
-        job_info["job_description"] = job_dict["description"].get("text")
-        job_info["job_title"] = job_dict["title"]
-        job_info["work_remote_allowed"] = job_dict["workRemoteAllowed"]
-        job_info["company_apply_url"] = (
-            job_dict["applyMethod"]
-            .get("com.linkedin.voyager.jobs.ComplexOnsiteApply")
-            .get("companyApplyUrl")
-        )
-        job_info["easy_apply_url"] = (
-            job_dict["applyMethod"]
-            .get("com.linkedin.voyager.jobs.ComplexOnsiteApply")
-            .get("easyApplyUrl")
-        )
-        job_info["formatted_location"] = job_dict["formattedLocation"]
-        job_info["listed_at"] = self.epoch_to_timestamp(job_dict["listedAt"])
-        return job_info
+    @staticmethod
+    def epoch_to_date(epoch_time_ms):
+        """Convert Unix epoch time in milliseconds to formatted string timestamp.
 
-    def get_jobs(self) -> Dict:
+        Args:
+            epoch_time_ms (int): Unix epoch time in milliseconds.
+
+        Returns:
+            str: Timestamp in 'YYYY-MM-DD HH:mm:ss' format.
+        """
+        epoch_time_s = epoch_time_ms / 1000
+
+        date_time = datetime.datetime.utcfromtimestamp(epoch_time_s)
+
+        formatted_date = date_time.strftime("%Y-%m-%d")
+
+        return formatted_date
+
+    @staticmethod
+    def export_list_of_dict_as_jsonl(data, filename) -> None:
+        with open(filename, "w", encoding="utf-8") as file:
+            for item in data:
+                json_line = json.dumps(item)
+                file.write(json_line + "\n")
+
+    @property
+    def get_api_client(self):
+        return Linkedin(self._login_email, self._login_password)
+
+    @property
+    def get_jobs(self) -> List:
         jobs = self._api_client.search_jobs(
             keywords=self.keywords,
             remote=self.remote,
@@ -86,13 +106,67 @@ class LinkedinJobParser:
         )
         return jobs
 
+    @property
+    def get_jobs_ids(self) -> List:
+        jobs_ids_list = [job["trackingUrn"].split(":")[-1] for job in self._jobs]
+        return jobs_ids_list
+
+    def get_information_from_job(self, job_dict: Dict) -> Dict:
+        job_info = {}
+        job_info["job_id"] = job_dict.get("jobPostingId")
+        job_info["company_name"] = (
+            job_dict.get("companyDetails", {})
+            .get(
+                "com.linkedin.voyager.deco.jobs.web.shared.WebCompactJobPostingCompany",
+                {},
+            )
+            .get("companyResolutionResult", {})
+            .get("name")
+        )
+        job_info["job_description"] = job_dict.get("description", {}).get("text")
+        job_info["job_title"] = job_dict.get("title")
+        job_info["work_remote_allowed"] = job_dict.get("workRemoteAllowed")
+        job_info["company_apply_url"] = (
+            job_dict.get("applyMethod", {})
+            .get("com.linkedin.voyager.jobs.ComplexOnsiteApply", {})
+            .get("companyApplyUrl")
+        )
+        job_info["easy_apply_url"] = (
+            job_dict.get("applyMethod", {})
+            .get("com.linkedin.voyager.jobs.ComplexOnsiteApply", {})
+            .get("easyApplyUrl")
+        )
+        job_info["formatted_location"] = job_dict.get("formattedLocation")
+        job_info["listed_at_timestamp"] = self.epoch_to_timestamp(
+            job_dict.get("listedAt", 0)
+        )
+        job_info["listed_at_date"] = self.epoch_to_date(job_dict.get("listedAt", 0))
+        return job_info
+
+    @sleep_and_retry
+    @limits(
+        calls=calls_per_period,
+        period=period_in_seconds,
+    )
+    def get_job(self, job_id: int) -> Dict:
+        return self._api_client.get_job(job_id)
+
+    def get_list_of_parsed_jobs(self) -> List:
+        list_of_parsed_jobs = []
+        logger.info(f"job ids: {self._job_ids}")
+        for job_id in self._job_ids:
+            job_dict = self.get_job(job_id)
+            jobs_info = self.get_information_from_job(job_dict)
+            logger.info("Job information: %s", jobs_info)
+            list_of_parsed_jobs.append(jobs_info)
+        return list_of_parsed_jobs
+
 
 if __name__ == "__main__":
     # env variables
     load_dotenv()
-    email = os.getenv("email")
-    password = os.getenv("password")
-    json_filename_export = "jobs.json"
+    email = os.environ.get("email")
+    password = os.environ.get("password")
 
     jobs_searcher = LinkedinJobParser(
         login_email=email,
@@ -100,16 +174,5 @@ if __name__ == "__main__":
         keywords="Data Engineer",
         days_old_listed_job=2,
     )
-
-    jobs = jobs_searcher.get_jobs()
-
-    # Create dict of values for jobs_list
-    # jobs_dict = {f"{index}": value for index, value in enumerate(jobs)}
-    # with open(json_filename_export, "w", encoding="utf-8") as f:
-    #     json.dump(jobs_dict, f, ensure_ascii=False, indent=4)
-
-    # get one particular job
-    # job = api.get_job("3894466400")
-    # job_info = get_information_from_job(job)
-    # with open("one_job_info.json", "w", encoding="utf-8") as f:
-    #     json.dump(job_info, f, ensure_ascii=False, indent=4)
+    jobs_info = jobs_searcher.get_list_of_parsed_jobs()
+    jobs_searcher.export_list_of_dict_as_jsonl(jobs_info, "jobs_info.jsonl")
--- python/tests/test_linkedin_parser.py
+++ python/tests/test_linkedin_parser.py
@@ -1,7 +1,68 @@
+import json
 from python_package.linkedin_parser import LinkedinJobParser
+from unittest.mock import patch, PropertyMock, mock_open
 
 
 def test_epoch_to_timestamp():
     epoch_time = 1712878212000
     expected = "2024-04-11 23:30:12"
     assert LinkedinJobParser.epoch_to_timestamp(epoch_time) == expected
+
+
+def test_epoch_to_date():
+    epoch_time = 1712878212000
+    expected = "2024-04-11"
+    assert LinkedinJobParser.epoch_to_date(epoch_time) == expected
+
+
+@patch.object(
+    LinkedinJobParser,
+    "get_jobs",
+    new_callable=PropertyMock,
+)
+@patch("python_package.linkedin_parser.LinkedinJobParser.get_api_client")
+def test_get_job_ids(p_client, p_get_jobs):
+    p_client.return_value = None
+    p_get_jobs.return_value = [
+        {"trackingUrn": "urn:li:jobPosting:3895256357"},
+        {"trackingUrn": "urn:li:jobPosting:3894466400"},
+    ]
+    jobs_searcher = LinkedinJobParser(
+        login_email="",
+        login_password="",
+        keywords="",
+        days_old_listed_job="",
+    )
+    assert jobs_searcher._job_ids == ["3895256357", "3894466400"]
+
+
+def test_export_as_jsonl():
+    # Prepare the data to be tested
+    data_to_test = [
+        {"name": "Alice", "age": 25, "city": "New York"},
+        {"name": "Bob", "age": 30, "city": "Los Angeles"},
+        {"name": "Charlie", "age": 35, "city": "Chicago"},
+    ]
+
+    # Expected JSON Lines output as a string
+    expected_output = "".join(json.dumps(item) + "\n" for item in data_to_test)
+
+    # Use mock_open to simulate file operations
+    with patch("builtins.open", mock_open()) as mocked_file:
+        # Call the function
+        LinkedinJobParser.export_list_of_dict_as_jsonl(data_to_test, "dummy_file.jsonl")
+
+        # Check that open was called correctly
+        mocked_file.assert_called_once_with("dummy_file.jsonl", "w", encoding="utf-8")
+
+        # Ensure all data was written correctly
+        written_data = "".join(
+            call.args[0] for call in mocked_file().write.call_args_list
+        )
+        assert (
+            written_data == expected_output
+        ), "The data written to the file does not match the expected output."
+
+
+def test_get_information_from_job():
+    pass
